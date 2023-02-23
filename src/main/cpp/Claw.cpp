d0c82af2409535fb483886d452d2c018b56b98c8
Alex Posey <alxposey@gmail.com>
2023-02-23 08:41:47 -0600

    Added Send data to slide and claw

--- src/main/cpp/Claw.cpp
+++ src/main/cpp/Claw.cpp
@@ -47,3 +47,18 @@ void Claw::GetError(){
 void Claw::GetRawError(){
     //returns raw error
 }
+
+void Claw::SendData(LoggingLevel verbose){
+    //sends data to dashboard with the enum LoggingLevel
+    switch(verbose){
+        case LoggingLevel::Everything: //everything that is not in the cases below it
+                                    //continue
+        case LoggingLevel::PID: //send PID (closed loop control) data
+                                    //continue
+        case LoggingLevel::Basic: //minimal useful data to driver
+                                    //continue
+        default: break; //make sure nothing else prints
+        
+
+    }
+}
--- src/main/cpp/Slide.cpp
+++ src/main/cpp/Slide.cpp
@@ -39,3 +39,18 @@ void Slide::GetError(){
 void Slide::GetRawError(){
     //returns raw error
 }
+
+void Slide::SendData(LoggingLevel verbose){
+    //sends data to dashboard with the enum LoggingLevel
+    switch(verbose){
+        case LoggingLevel::Everything: //everything that is not in the cases below it
+                                    //continue
+        case LoggingLevel::PID: //send PID (closed loop control) data
+                                    //continue
+        case LoggingLevel::Basic: //minimal useful data to driver
+                                    //continue
+        default: break; //make sure nothing else prints
+        
+
+    }
+}
